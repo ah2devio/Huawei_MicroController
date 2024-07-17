0c3fcf02f7d41ce8872fa08bbc2f04a4fdcb195b
Ah2devio <54669173+ah2devio@users.noreply.github.com>
2024-07-17 23:43:40 +0300

    _SET TUT EX

---
+++ Tutorial Examples/L3_July17/Assignment Operators/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 5;
+    printf("%d\n",i++);//5
+    printf("%d\n",i--);//6
+    printf("%d\n",i);//5
+    printf("Hello world!\n");
+    return 0;
+}
